0513eb90f4983627c7c1b2d20306d2c7b8b43088
Kyu-u <melanchthonbutarbutar@gmail.com>
2021-11-16 19:38:16 +0700

    aa

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      "pinkaa": "#FB2e86",
+      "redaa": "#f92545",
+      "blueaa": "#2E19C4",
+      "offnavyblue": "#3A519B",
+      "navyblue": "#151875",
+     "skyblue": "#F2F7FF",
+      "pantonepurple": "#E1D1F6",
+      "purpleaa": "#7E33E0",
+      "offpurple": "#9F64B4",
+    },
+    fontFamily: {
+      'josefin': ['Josefin Sans','sans-serif'],
+      'lato': ['Lato','sans-serif']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
